eabe0cecb8447d33e9ad24cb2caca7e62cb96032
pengcheng.chen <pengcheng.chen@edetek.com>
2022-02-08 18:58:29 +0800

    add the cur_read

--- nene/helper.py
+++ nene/helper.py
@@ -9,6 +9,15 @@ class FileHelper(object):
             return f.read()
 
     @classmethod
+    def cur_read(cls, file):
+        """
+        读取同一路径下的文件
+        :param file: 文件名
+        :return:
+        """
+        return cls.read(os.path.curdir, file)
+
+    @classmethod
     def replace(cls, rep: dict, *path):
         with open(os.path.join(*path), 'r') as f:
             return f.read().format(**rep)
