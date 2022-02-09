5721bcb3ae775301174aae10690aa05cc9615489
pengcheng.chen <pengcheng.chen@edetek.com>
2022-02-09 14:09:06 +0800

    change the path

--- tests/main_app.py
+++ tests/main_app.py
@@ -1,4 +1,4 @@
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(["nene/test_meta_mysql.py", "test_context_manager.py"])
+    pytest.main(["nene/test_meta_mysql.py", "common/test_context_manager.py"])
