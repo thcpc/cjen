35fa38ea47407438d663ac25dda6a27d6fe60bf7
pengcheng.chen <pengcheng.chen@edetek.com>
2022-02-14 10:50:49 +0800

    rename the test package name

--- tests/nene/employee.sql
+++
@@ -1,5 +0,0 @@
-SELECT e.id as id, e.name as name, c.name as company
-FROM employees as e
-LEFT JOIN company as c
-on e.company_id = c.id
-WHERE e.id = %(id)s;
--- tests/nene/employees_of_company.sql
+++
@@ -1,5 +0,0 @@
-SELECT e.id as id, e.name as name, c.name as company
-FROM company as c
-LEFT JOIN employees as e
-on c.id = e.company_id
-WHERE c.id=%(id)s;
